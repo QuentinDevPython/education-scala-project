2887a2f323e5aceb743eecc7a5b1bb65a2789d8b
BaptisteBtx <baptiste.bontoux@gmail.com>
2022-10-06 17:57:44 +0200

    Domain class (union,intersect,diff,empty,singleton)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,17 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+@main
+def main: Unit =
 
-def msg = "I was compiled by Scala 3. :)"
+  case class Domain[A](values: Set[A]):
+    def union(domain: Domain[A]): Domain[A] = Domain(values.union(domain.values))
+    def union(value: A): Domain[A] = union(domain = Domain[A](Set(value)))
+
+    def intersect(domain: Domain[A]): Domain[A] = Domain(values.intersect(domain.values))
+    def intersect(value: A): Domain[A] = intersect(domain = Domain[A](Set(value)))
+
+    def diff(domain: Domain[A]): Domain[A] = Domain(values.diff(domain.values))
+    def diff(value: A): Domain[A] = diff(domain = Domain[A](Set(value)))
+
+    def isEmpty: Boolean = values.isEmpty
+    def isSingleton: Boolean = values.size == 1
+
+
