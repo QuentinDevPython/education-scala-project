e95c1cbbca24647dc197c6236c74200653f56062
BaptisteBtx <baptiste.bontoux@gmail.com>
2022-10-13 09:29:36 +0200

    EqualVariables and EqualConstant + Recursive solve

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+
 @main
 def main: Unit =
 
@@ -14,4 +15,82 @@ def main: Unit =
     def isEmpty: Boolean = values.isEmpty
     def isSingleton: Boolean = values.size == 1
 
+  case class Variable[A](name: String)
+
+  enum Constraint[A]:
+    case EqualVariables(x: Variable[A], y: Variable[A])
+    case EqualConstant(x: Variable[A], c: A)
+    case DiffVariables(x: Variable[A], y: Variable[A])
+    case DiffConstant(x: Variable[A], c: A)
+    case AllDiff(variables: List[Variable[A]])
+
+
+
+
+  case class CSP[A](domains: Map[Variable[A], Domain[A]], constraints: Set[Constraint[A]]):
+    def solve: Map[Variable[A], Domain[A]] =
+      val constraint: Option[Constraint[A]] = constraints.find(c => !is_satisfied(c)) // Cherche la première contrainte non satisfaite
+      constraint match
+        case None => domains // Si elles le sont toutes on a trouvé notre Map
+        case Some(x) => // Si une contrainte non satisfaite a été trouvée
+          // On créer un nouveau CSP qu'on résout (récursivement)
+          val newCSP: CSP[A] = update_domains(apply_constraint(x))
+          newCSP.solve
+
+    def is_satisfied(constraint: Constraint[A]): Boolean =
+      // Vérifie si la contrainte est satisfaite ou non
+      constraint match
+        case Constraint.EqualVariables(x, y) =>
+          are_domains_equal(domains(x),domains(y))
+        case Constraint.EqualConstant(x, c) =>
+          are_domains_equal(domains(x), Domain(Set(c)))
+        case _ => true
+
+    def apply_constraint(constraint: Constraint[A]): Map[Variable[A], Domain[A]] =
+      // Applique la contrainte aux variables concernées
+      // Renvoie une Map qui permet de mettre à jour les "domains"
+      constraint match
+        case Constraint.EqualVariables(x, y) =>
+          val newDomain: Domain[A] = set_domains_equal(domains(x), domains(y))
+          Map(x -> newDomain, y -> newDomain)
+        case Constraint.EqualConstant(x, c) =>
+          Map(x -> set_domains_equal(domains(x), Domain(Set(c))))
+        case _ => Map() // Contrainte inconnue
+
+
+    def update_domains(newDomains: Map[Variable[A], Domain[A]]): CSP[A] =
+      // Créer un nouvel objet CSP avec les domaines mis a jour
+      copy(domains=domains++newDomains)
+    def set_domains_equal(x: Domain[A], y: Domain[A]): Domain[A] =
+      // Renvoie l'intersection des domain x et y
+      x.intersect(y)
+    def are_domains_equal(x:Domain[A], y:Domain[A]): Boolean =
+      // True si les domaines sont égaux
+      x.values.equals(y.values)
+
+  //TESTS
+  val dom1: Domain[Int] = Domain[Int](Set(1, 2, 3))
+  //val dom2: Domain[Int] = Domain[Int](Set(1, 2, 3))
+  //val dom3: Domain[Int] = Domain[Int](Set(1, 2, 3))
+
+  val v1 = Variable[Int]("v1")
+  val v2 = Variable[Int]("v2")
+  val v3 = Variable[Int]("v3")
+
+  val colorCsp: CSP[Int] =
+    CSP(
+      domains =
+      // Each variable is bound to the same domain
+        Map(
+          v1 -> dom1,
+          v2 -> dom1,
+          v3 -> dom1
+        ),
+      constraints =
+        Set(
+          Constraint.EqualVariables(v1,v2),
+          Constraint.EqualConstant(v2,2)
+        )
+    )
+  print(colorCsp.solve)
 
